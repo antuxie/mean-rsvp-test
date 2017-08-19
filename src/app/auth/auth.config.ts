6df6c38ecd7f76ac298ce01d9a19218961e7f751
antu xie <antuxie@gmail.com>
2017-08-19 12:59:48 -0500

    new commit

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -15,7 +15,7 @@ export const AUTH_CONFIG: AuthConfig = {
   CLIENT_DOMAIN: 'eddiezhazha.auth0.com', // e.g., kmaida.auth0.com
   AUDIENCE: 'http://localhost:8083/api/', // e.g., http://localhost:8083/api/
   REDIRECT: `${ENV.BASE_URI}/callback`,
-  SILENT_REDIRECT: '${ENV.BASE_URI}/silent', // ${ENV.BASE_URI}/silent on PRODUCTION
+  SILENT_REDIRECT: `${ENV.BASE_URI}/silent`, // ${ENV.BASE_URI}/silent on PRODUCTION
   SCOPE: 'openid profile',
   NAMESPACE: 'http://myapp.com/roles'
 };
